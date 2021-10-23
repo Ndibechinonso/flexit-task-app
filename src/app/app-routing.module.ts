7d7047a0c377454e1c3977eb20832286a4359162
Ndibe Chinonso <edwardndibe@gmail.com>
2021-10-23 15:03:42 +0100

    activities commit BUILD ERROR FIX 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   //   loadChildren: () =>
   //     import('./side-nav/side-nav-routing.module').then((m) => m.SectionRoutingModule),
   // },
-  { path: '', component: GalleryGridComponent },
+  { path: 'home', component: GalleryGridComponent },
  ];
 
 @NgModule({
