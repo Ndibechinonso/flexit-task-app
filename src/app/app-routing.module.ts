411ad7052278e633a79ba09136a6fc12b0666c61
Ndibe Chinonso <edwardndibe@gmail.com>
2021-10-23 15:11:11 +0100

    undo commit to 238e3d1198ca444ec6f5bf617ac34381a802dbf2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GalleryGridComponent} from "./gallery-grid/gallery-grid.component";
 import {OtherRouteComponent} from "./other-route/other-route.component";
-// import {SectionRoutingModule} from "./side-nav/side-nav-routing.module";
 
-const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () =>
-  //     import('./side-nav/side-nav-routing.module').then((m) => m.SectionRoutingModule),
-  // },
-  { path: 'home', component: GalleryGridComponent },
- ];
+const routes: Routes = [{ path: '', component: GalleryGridComponent },
+  { path: 'other', component: OtherRouteComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
