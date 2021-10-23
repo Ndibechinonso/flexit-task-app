3d20d21dc63f09e2ae21ef3e9b2cd3401c9db921
Ndibe Chinonso <edwardndibe@gmail.com>
2021-10-23 14:58:58 +0100

    activities commit BUILD ERROR FIX 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GalleryGridComponent} from "./gallery-grid/gallery-grid.component";
 import {OtherRouteComponent} from "./other-route/other-route.component";
-import {SectionRoutingModule} from "./side-nav/side-nav-routing.module";
+// import {SectionRoutingModule} from "./side-nav/side-nav-routing.module";
 
 const routes: Routes = [
   // {
--- src/app/side-nav/side-nav-routing.module.ts
+++ src/app/side-nav/side-nav-routing.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { GalleryGridComponent} from "../gallery-grid/gallery-grid.component";
-import {SideNavComponent} from "./side-nav.component";
-import {OtherRouteComponent} from "../other-route/other-route.component";
-
-
-const routes: Routes = [
-  {
-    path: '',
-    component: SideNavComponent,
-    children: [
-      { path: '', redirectTo: '' },
-      { path: 'other', redirectTo: '' },
-      { path: '**', redirectTo: '' }, // Add 404 maybe
-    ],
-  },
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class SectionRoutingModule {}
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { GalleryGridComponent} from "../gallery-grid/gallery-grid.component";
+// import {SideNavComponent} from "./side-nav.component";
+// import {OtherRouteComponent} from "../other-route/other-route.component";
+//
+//
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: SideNavComponent,
+//     children: [
+//       { path: '', redirectTo: '' },
+//       { path: 'other', redirectTo: '' },
+//       { path: '**', redirectTo: '' }, // Add 404 maybe
+//     ],
+//   },
+// ];
+//
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule],
+// })
+// export class SectionRoutingModule {}
